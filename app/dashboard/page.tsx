316253a3420f12f12e192fe95b55a4bf5ce85844
Kinjal Dixit <kinjald1207@gmail.com>
2026-06-22 00:28:59 +0530

    fixes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const DashboardMainPage = () => {
-  return (
-    <div>DashboardMainPage</div>
-  )
-}
-
-export default DashboardMainPage
-=======
 import AddNewButton from "@/features/dashboard/components/add-new-btn";
 import AddRepo from "@/features/dashboard/components/add-repo";
 
 import ProjectTable from "@/features/dashboard/components/project-table";
-import { getAllPlaygroundForUser , deleteProjectById ,editProjectById , duplicateProjectById} from "@/features/playground/actions";
+import {
+  getAllPlaygroundForUser,
+  deleteProjectById,
+  editProjectById,
+  duplicateProjectById,
+} from "@/features/playground/actions";
 
 const EmptyState = () => (
   <div className="flex flex-col items-center justify-center py-16">
@@ -25,18 +19,18 @@ const EmptyState = () => (
 
 const DashboardMainPage = async () => {
   const playgrounds = await getAllPlaygroundForUser();
-  console.log(playgrounds);
+
   return (
     <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
         <AddNewButton />
         <AddRepo />
       </div>
+
       <div className="mt-10 flex flex-col justify-center items-center w-full">
         {playgrounds && playgrounds.length === 0 ? (
           <EmptyState />
         ) : (
-          // @ts-ignore
           <ProjectTable
             projects={playgrounds || []}
             onDeleteProject={deleteProjectById}
@@ -50,4 +44,3 @@ const DashboardMainPage = async () => {
 };
 
 export default DashboardMainPage;
->>>>>>> 372e3bc1481a84dc0ffee574b447818c0c2758b9
